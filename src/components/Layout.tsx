e8c6ac1105356adbf79ff503cbc341970ec77ed4
N <botakjr8888@gmail.com>
2024-09-27 06:57:42 +0700

    stylesheet code splitting

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Style, cx } from "hono/css";
 import { PropsWithChildren } from "hono/jsx";
-import * as styles from "../styles";
+import styles from "../styles";
 
 const TITLE = "Data Passport/Visa PSG";
 
@@ -16,9 +16,12 @@ export default function(props: PropsWithChildren<LayoutProps>) {
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{TITLE}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
         <Style />
       </head>
-      <body class={cx(styles.layout, props.class)}>
+      <body class={cx(styles, props.class)}>
         {props.children}
       </body>
       <script src="https://unpkg.com/htmx.org@2.0.2"
